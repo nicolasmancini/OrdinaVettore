bec2cf83b48b9c9c635890160399645a034b7aa4
Nicolas Mancini <NICOLAS.MANCINI@local.ittcesena.it>
2021-10-08 08:35:29 +0200

    Aggiunta commenti

--- OrdinamentoVettori/OrdinamentoVettori/Program.cs
+++ OrdinamentoVettori/OrdinamentoVettori/Program.cs
@@ -6,9 +6,10 @@ namespace OrdinamentoVettori
     {
         static void Main(string[] args)
         {
+            //Creazione e popolamento del vettore di interi
             int [] vettore = new int [10] { 11, 55, -9, 7, 74, 147, -87, 6, 0, 18};
 
-
+            //Invoco il metodo per l'ordinamento
             ordina(vettore);
 
             for (int i = 0; i < 10; i++)
@@ -17,6 +18,7 @@ namespace OrdinamentoVettori
             Console.ReadKey();
         }
 
+        //Metodo per l'ordinamento
         static void ordina (int[] vettore)
         {
             Array.Sort(vettore);
